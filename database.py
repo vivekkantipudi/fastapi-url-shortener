0d3c63a32fbab02f457e1b12765d11e41f881c18
vivekkantipudi <147791906+vivekkantipudi@users.noreply.github.com>
2026-01-20 09:43:13 +0530

    Update database.py

--- database.py
+++ database.py
@@ -1,9 +1,17 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
+from dotenv import load_dotenv
 
-# Change 'root' and 'password' to your MySQL credentials
-SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:ViveK%402005@localhost/url_db"
+load_dotenv()
+
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_HOST = os.getenv("DB_HOST")
+DB_NAME = os.getenv("DB_NAME")
+
+SQLALCHEMY_DATABASE_URL = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
